bdea5da8a0d37df9573a52eaa7f98095303cbd5b
tim <timothy.potts@colorado.edu>
2019-11-16 10:30:48 -0700

    init commit

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var path = require("path");
+
+
+var app = express();
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+
+var reservations = [];
