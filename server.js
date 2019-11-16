9b39537a55687bdc5f265f8f24a84caad9a43962
tim <timothy.potts@colorado.edu>
2019-11-16 11:46:58 -0700

    json derillo

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 var express = require("express");
 var path = require("path");
 
+var PORT = 3000;
 
 var app = express();
 
+
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+
